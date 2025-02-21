be129f8121330a2f41450c3fa3545f9ec5f4190e
Eskaliu <157012890+Eskaliu@users.noreply.github.com>
2025-02-21 14:44:11 +0100

    FinishClean

--- cascade.lean
+++ cascade.lean
@@ -73,11 +73,6 @@ lemma icc_succ (s k : Nat) (sk: s ≤ k) (a : Nat → Nat):
     left
     exact sk
 
---#check Finset.sum_Ico_succ_top
-/- #check Finset.Icc_eq_cons_Ico
-#check Finset.sum_cons
- -/
-
 -- Zeige kein Binom mit x oben und x>=m ist x uber k > m
 -- um Zu beweisen das Finset.max oben auch max findet
 lemma a_k_maximiser (k m : Nat) (hk : 1 ≤ k) (hm : 1 ≤ m) : ∀ x , Nat.choose x k ≤ m → x ≤ a_k k m hm :=
@@ -95,14 +90,6 @@ lemma a_k_maximiser (k m : Nat) (hk : 1 ≤ k) (hm : 1 ≤ m) : ∀ x , Nat.choo
     have name := help_7 k m x hk q1.right q1.left  
     apply not_le_of_lt name
     exact hx
-
---#check max_le_iff
-
-
-
---#check Nat.eq_add_of_sub_eq
--- a = m ; b = a_k ... ; c = 0
--- hle mit
 
 
 -- das gefundene Max Elem erfuellt die Eingeschaft ≤ m 
@@ -146,7 +133,7 @@ have sum_ge_ak : ∑ i ∈ Finset.Icc s k, (a i).choose i + (a_k (k + 1) m hm).c
   rw [Finset.Icc_self, Finset.sum_singleton] at this
   exact this
 rw [← right] at sum_ge_ak
---widerspruch da .................................................
+--widerspruch da ...
 have toChoose := Nat.choose_mono k contra
 dsimp at toChoose
 have more := le_trans (Nat.add_le_add_right toChoose _) sum_ge_ak
@@ -154,28 +141,6 @@ rw [← Nat.choose_succ_succ] at more
 have almost := a_k_maximiser (k+1) m (by exact Nat.le_add_right_of_le q1) hm _ more
 linarith
 
--- (a_k (k + 1) m hm).choose (k + 1) + a k).choose k + (a_k (k + 1) m hm).choose (k + 1) > m weil m = ... ist Beweise ! 
-#print Monotone
-
-#check Nat.add_le_add_right
-
-
-/- 
-  --simp only [ge_iff_le]
-  have ge_zero {i : Nat} (h : i ∈ Finset.Icc s k) : 0 ≤ (fun i => (a i).choose i) i := 
-  by
-    exact Nat.zero_le ((a i).choose i)
-  
-  apply @Finset.single_le_sum _ _ _ (fun i => (a i).choose i) (Finset.Icc s k) (by apply ge_zero ) _
-  rw [Finset.mem_Icc]
-  simp_all only [Finset.mem_Icc, zero_le, implies_true, le_refl, and_self] --(Finset.exists_le_maximal)
-    
-  rel [sum_ge_ak_help] -/
-#check Nat.eq_add_of_sub_eq
-  
-
-#check Finset.sum_le_sum_of_subset
-#check Finset.Icc_self
 
 -- Hier steht der Beweis zum k-kaskade Lemma
 /--∀m,k ∈ ℕ, 1 ≤ m ∃s: 1 ≤ s ∧ s ≤ k : ∃ Folge mit aₖ ≥ ...≥ aₛ für welches gilt
@@ -345,8 +310,6 @@ theorem cascade (k : Nat) (hk : 1 ≤ k) :
                   have x_eq_y : x = y := by linarith
                   subst x_eq_y
                   simp_all only [lt_self_iff_false]
-            /- Hier noch ein paar Kommentare maybe ---------------------------------------------------------------
-             -/
             · constructor
               · -- s ≤ k gilt (Aussage u) 
                 rw [if_pos u]
@@ -366,8 +329,6 @@ theorem cascade (k : Nat) (hk : 1 ≤ k) :
                   
                 · exact u
               
-
-
       -- Fall 1 > k ==> k = 0 (da k ∈ ℕ )
     · simp only [not_le, Nat.lt_one_iff] at q1
       rw [q1]
@@ -384,97 +345,3 @@ theorem cascade (k : Nat) (hk : 1 ≤ k) :
         · simp only [zero_add, Finset.Icc_self, Finset.sum_singleton, ↓reduceIte,
           Nat.choose_one_right] -- ref_4
 
-
-
-
-
-
-
-#exit
-#check Set.Icc_eq_singleton_iff
-
--- # ref_3
-
-#check if_pos
-
--- # ref_2 ref_4
-
-#check Finset.Icc_self
-#check Set.strictMonoOn_singleton
-#check Finset.sum_singleton
-#check if_pos
-#check Nat.choose_one_right
-
-
-lemma help_maxi (k m x: ℕ ) (hm : 1 ≤ m) : x < m + 1 → m < x.choose k :=
-  by
-  induction' m with m md
-  · intro a
-    simp_all only [nonpos_iff_eq_zero, one_ne_zero]
-  · intro a
-    
-    sorry
-
-
--- # ref_7
-
--- kann `x` in `Finset.range (m+1)` sein ?
-#check Finset.mem_filter
-
--- anschließend als ungleichung azsdrücken, mit
-#check Finset.mem_range
-
--- dann help_7
-
--- mit
-#check Nat.lt_iff_add_one_le
-#check Nat.choose_succ_self_right
-#check Nat.choose_le_choose
-
--- abschließen mit
-#check Nat.not_le_of_lt
-#check False.elim
--- oder bei letzterem di tactic `exfalso`
-
-#check False.elim
-
-#check not_le_of_lt
-
---Im Hauptbeweis können 2 Fälle passieren, laut
-#check le_iff_eq_or_lt
-
--- Im Fall von = können wir s := (k+1) und a := (fun x => if x = (k+1) then (a_k (k+1) m sorry) else 0)
--- setzen und schnell fertig sein
--- Adernfalls können wir ↓ nutzen
-#check Nat.sub_pos_iff_lt
-#check Nat.lt_iff_add_one_le
--- und erhalten dass `1 ≤ m - (Nat.choose (a_k (k+1) m sorry) (k+1))`
-
--- Bennen wir `1 ≤ m - (Nat.choose (a_k (k+1) m sorry) (k+1))` *dif*
--- und merken dass man `ih` in *dif* spezialisieren kann
--- Dann gibt `ih` uns ein `s` und eine sequenz `a` (die aber nur auf [s,k] monoton ist, nicht [s,(k+1)])
--- und die Tatsache dass `m - (Nat.choose (a_k (k+1) m sorry) (k+1)) = ∑ i ∈ [s,k], Nat.choose ...`
-
--- Wir wollen dieses `s` nutzen, und die sequenz erweitern durch:
--- A := `fun x => if x ≤ k then a x else (a_k (k+1) m sorry)`
--- Dann, mit der vorherigen gleichung und:
--- `(Nat.choose (a_k (k+1) m sorry) (k+1)) + (∑ i ∈ [s,k], Nat.choose (a i) i) = ∑ i ∈ [s,(k+1)], Nat.choose (A i) i`
--- (welches wir beweisen müssen), hätten wir unsere gewünschte cascade darstellung für k+1
-
--- Außerdem muss dann bewiesen werden dass `A k < A (k+1)` für die stricte monotonie
--- Das lemma das im Buch genutz wird ist
-#check Nat.choose_succ_succ
--- und sein "contradcitig maximality of aₖ" ist unser lemma "a_k_maximiser" und
-#check Nat.not_succ_le_self
--- # ref_5
-
--- use 0, dann
-#check Finset.mem_filter
-#check Finset.mem_range
-#check Nat.zero_le
--- cases' auf k, dann
-#check Nat.choose_zero_right
-#check Nat.choose_zero_succ
-
--- # ref_6
-#check Finset.le_max'
